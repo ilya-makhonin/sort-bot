e8e2eca4c69ba1cab846c8b287783387b588fc22
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-26 01:05:46 +0300

    Used state. Update code and add message_handler's

--- bot.py
+++ bot.py
@@ -1,24 +1,10 @@
 import telebot
 from sql import sql_method as db
-from configuration import *
+from constants import *
 import config
 
 
 bot = telebot.TeleBot(config.token)
-
-# Cash for speed (don't realize)
-cash = {
-    'author': None,
-    'theme': None,
-    'level': None
-}
-
-
-def get_db_information():
-    cash['author'] = db.get_info_by_choice('author')
-    cash['theme'] = db.get_info_by_choice('theme')
-    cash['level'] = db.get_info_by_choice('level')
-# ******************************
 
 
 def get_markup(buttons, rows=1):
@@ -33,10 +19,28 @@ def pre_modified_button(buttons, level=False):
         result.append(level)
     for button in buttons:
         result.append(button[-1])
+    if len(result) > 50:
+        result = result[0:50]
     return get_markup(result)
 
 
+@bot.message_handler(commands=['start'])
+def start_handler(message: telebot.types.Message):
+    name = message.from_user.first_name
+    if name is None:
+        name = message.from_user.last_name
+    db.add_user(message.from_user.id, message.from_user.username, name)
+    db.change_state('starting', message.from_user.id)
+    bot.send_message(
+        message.from_user.id,
+        text=start_message,
+        parse_mode='markdown',
+        reply_markup=get_markup(main_menu))
+
+
+@bot.message_handler(regexp=first_level_back)
 def back_to_main_menu(message: telebot.types.Message):
+    db.change_state('starting', message.from_user.id)
     bot.send_message(
         message.from_user.id,
         'Go back, bro!',
@@ -48,101 +52,83 @@ def back_to_main_menu(message: telebot.types.Message):
         reply_markup=get_markup(main_menu))
 
 
-@bot.message_handler(commands=['start'])
-def start_handler(message: telebot.types.Message):
-    name = message.from_user.first_name
-    if name is None:
-        name = message.from_user.last_name
-    db.add_user(message.from_user.id, message.from_user.username, name)
-    bot.send_message(
-        message.from_user.id,
-        text=start_message,
-        parse_mode='markdown',
-        reply_markup=get_markup(main_menu))
-
-
 @bot.message_handler(regexp='По авторам')
 def author_handler(message: telebot.types.Message):
+    db.change_state('author', message.from_user.id)
     bot.send_message(
         message.from_user.id,
         text=hello_author_mes,
         parse_mode='markdown',
         reply_markup=pre_modified_button(db.get_info_by_choice('author'), first_level_back)
     )
-    bot.register_next_step_handler_by_chat_id(message.from_user.id, author_list_articles)
 
 
+@bot.message_handler(func=lambda message: db.get_state(message.from_user.id) == 'author')
 def author_list_articles(message: telebot.types.Message):
-    if message.text == first_level_back:
-        back_to_main_menu(message)
-    else:
-        bot.send_message(
-            message.from_user.id,
-            text=author_mes.format(message.text),
-            parse_mode='markdown',
-            reply_markup=pre_modified_button(db.get_articles('author', message.text), back_to_author))
-        bot.register_next_step_handler_by_chat_id(message.from_user.id, get_article_author)
+    db.change_state('author_choice', message.from_user.id)
+    bot.send_message(
+        message.from_user.id,
+        text=author_mes.format(message.text),
+        parse_mode='markdown',
+        reply_markup=pre_modified_button(db.get_articles('author', message.text), back_to_author))
 
 
+@bot.message_handler(func=lambda message: db.get_state(message.from_user.id) == 'author_choice')
 def get_article_author(message: telebot.types.Message):
     if message.text == back_to_author:
+        db.change_state('author', message.from_user.id)
         author_handler(message)
     else:
         article = db.get_article(message.text)
         bot.send_message(message.from_user.id, '{} - {}'.format(article[0], article[1]))
-        bot.register_next_step_handler_by_chat_id(message.from_user.id, get_article_author)
 
 
 @bot.message_handler(regexp='По темам')
 def theme_handler(message: telebot.types.Message):
+    db.change_state('theme', message.from_user.id)
     bot.send_message(
         message.from_user.id,
         text=hello_theme_mes,
         parse_mode='markdown',
         reply_markup=pre_modified_button(db.get_info_by_choice('theme'), first_level_back)
     )
-    bot.register_next_step_handler_by_chat_id(message.from_user.id, theme_list_articles)
 
 
+@bot.message_handler(func=lambda message: db.get_state(message.from_user.id) == 'theme')
 def theme_list_articles(message: telebot.types.Message):
-    if message.text == first_level_back:
-        back_to_main_menu(message)
-    else:
-        bot.send_message(
-            message.from_user.id,
-            text=theme_mes.format(message.text),
-            parse_mode='markdown',
-            reply_markup=pre_modified_button(db.get_articles('theme', message.text), back_to_theme))
-        bot.register_next_step_handler_by_chat_id(message.from_user.id, get_article_theme)
+    db.change_state('theme_choice', message.from_user.id)
+    bot.send_message(
+        message.from_user.id,
+        text=theme_mes.format(message.text),
+        parse_mode='markdown',
+        reply_markup=pre_modified_button(db.get_articles('theme', message.text), back_to_theme))
 
 
+@bot.message_handler(func=lambda message: db.get_state(message.from_user.id) == 'theme_choice')
 def get_article_theme(message: telebot.types.Message):
     if message.text == back_to_theme:
+        db.change_state('theme', message.from_user.id)
         theme_handler(message)
     else:
         article = db.get_article(message.text)
         bot.send_message(message.from_user.id, '{} - {}'.format(article[0], article[1]))
-        bot.register_next_step_handler_by_chat_id(message.from_user.id, get_article_theme)
 
 
 @bot.message_handler(regexp='Я сам выберу')
 def all_articles_handler(message: telebot.types.Message):
+    db.change_state('all_articles', message.from_user.id)
     bot.send_message(
         message.from_user.id,
         text=hello_all_mes,
         parse_mode='markdown',
         reply_markup=pre_modified_button(db.get_articles('all'), first_level_back))
-    print()
-    bot.register_next_step_handler_by_chat_id(message.from_user.id, all_list_articles)
 
 
+@bot.message_handler(func=lambda message: db.get_state(message.from_user.id) == 'all_articles')
 def all_list_articles(message: telebot.types.Message):
-    if message.text == first_level_back:
-        back_to_main_menu(message)
-    else:
-        article = db.get_article(message.text)
-        bot.send_message(message.from_user.id, '{} - {}'.format(article[0], article[1]))
-        bot.register_next_step_handler_by_chat_id(message.from_user.id, all_list_articles)
+    article = db.get_article(message.text)
+    bot.send_message(message.from_user.id, '{} - {}'.format(article[0], article[1]))
+    bot.register_next_step_handler_by_chat_id(message.from_user.id, all_list_articles)
 
 
 if __name__ == '__main__':
