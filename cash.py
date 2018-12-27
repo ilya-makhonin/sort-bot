cc2de004a8d86ae6cb5d1ef584f85a8faa2c3c65
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-27 23:07:22 +0300

    Cash prototype

--- cash.py
+++ cash.py
@@ -1,16 +1,20 @@
 from sql import sql_method as db
 
 
-# Cash for speed (don't realize)
-cash = {
-    'author': None,
-    'theme': None,
-    'level': None
-}
+class Cash:
+    def __init__(self):
+        self.cash = {
+            'users': tuple,
+            'author': tuple,
+            'theme': tuple,
+            'level': tuple
+        }
+
+    def get_db_information(self):
+        self.cash['users'] = db.get_info_by_choice('users')
+        self.cash['author'] = db.get_info_by_choice('author')
+        self.cash['theme'] = db.get_info_by_choice('theme')
+        self.cash['level'] = db.get_info_by_choice('level')
 
 
-def get_db_information():
-    cash['author'] = db.get_info_by_choice('author')
-    cash['theme'] = db.get_info_by_choice('theme')
-    cash['level'] = db.get_info_by_choice('level')
-# ******************************
+cash = Cash()
