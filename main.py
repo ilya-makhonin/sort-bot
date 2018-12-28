1b6a0b83092fa3fe950224302e15c177c3c64665
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-28 22:29:00 +0300

    Add func for to create server and start app

--- main.py
+++ main.py
@@ -6,6 +6,57 @@ import flask
 from config import *
 
 
+def flask_init(bot_object):
+    webhook_app = flask.Flask(__name__)
+    webhook_logger = webhook_app.logger
+    # webhook_logger.setLevel(log.levels.get('DEBUG'))
+    # webhook_logger.addHandler(log.__file_handler('logs.log', log.__get_formater()))
+
+    @webhook_app.route('/', methods=['GET', 'HEAD'])
+    def index():
+        return ''
+
+    @webhook_app.route('token', methods=['POST'])
+    def webhook():
+        if flask.request.headers.get('content-type') == 'application/json':
+            json_string = flask.request.get_data().decode('utf-8')
+            update = telebot.types.Update.de_json(json_string)
+            bot_object.process_new_updates([update])
+            # webhook_logger.debug('updates from webhook: ' + str(update))
+            return ''
+        else:
+            flask.abort(403)
+
+    return webhook_app
+
+
+def start(use_webhook=False, **webhook_data):
+    # logger = log.logger('main', 'logs.log')
+    try:
+
+        bot_object = bot.bot_start(use_webhook=use_webhook, webhook_data=webhook_data)
+
+        if use_webhook:
+            server_app = flask_init(bot_object)
+            return server_app
+
+    except Exception as err:
+        pass
+        # logger.exception('bot crashed')
+        #logger.exception(err)
+
+
+if __name__ == '__main__':
+    app = start(
+        use_webhook=True,
+        webhook_ip='194.32.77.67',
+        webhook_port='webhook_port',
+        token='token',
+        ssl_cert='ssl_cert'
+    )
+
+
+"""
 def create_server():
     logger = telebot.logger
     telebot.logger.setLevel(logging.INFO)
@@ -41,3 +92,4 @@ def start_server():
 
 if __name__ == '__main__':
     start_server()
+"""
