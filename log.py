36da6f9464b0c1827829633b09491e90b471a4ad
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-28 23:16:09 +0300

    Func for logging

---
+++ log.py
@@ -0,0 +1,27 @@
+import logging
+
+LEVELS = {
+    'DEBUG': logging.DEBUG,
+    'CRITICAL': logging.CRITICAL,
+    'INFO': logging.INFO,
+    'WARNING': logging.WARNING
+}
+
+
+def __get_formater():
+    return logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def __file_handler(file_name, formatter):
+    handler = logging.FileHandler(file_name)
+    handler.setFormatter(formatter)
+    return handler
+
+
+def logger(name, file_name, log_level='DEBUG'):
+    logger = logging.getLogger(name)
+    logger.setLevel(LEVELS.get(log_level))
+    formatter = __get_formater()
+    handler = __file_handler(file_name, formatter)
+    logger.addHandler(handler)
+    return logger
