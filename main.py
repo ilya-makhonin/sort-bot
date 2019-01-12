f6b72de6a9df736937aa6f7900dd723cb2648e86
Ilia Makhonin <noizemkutane77@gmail.com>
2019-01-12 04:00:39 +0300

    Changed the function to initialize the server and install web hooks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import telebot
-from bot import bot
+import bot
 import flask
 from config import *
 import log
@@ -32,20 +32,17 @@ def flask_init(bot_object):
 def start(use_webhook=False, **webhook_data):
     logger = log.logger('main', 'main.log')
     try:
-        bot_object = bot.bot_start(use_webhook=use_webhook, webhook_data=webhook_data)
+        bot_object = bot.bot_start(webhook_data=webhook_data, use_webhook=use_webhook, logging_enable=False)
         if use_webhook:
-            server_app = flask_init(bot_object)
-            return server_app
+            server = flask_init(bot_object)
+            return server
+        return None
     except Exception as err:
         logger.exception('bot crashed')
         logger.exception(err)
 
 
+app = start(use_webhook=False, webhook_ip=WEBHOOK_HOST, webhook_port=WEBHOOK_PORT, token=token, ssl_cert=SSL_SERT)
+
 if __name__ == '__main__':
-    app = start(
-        use_webhook=True,
-        webhook_ip=WEBHOOK_HOST,
-        webhook_port=WEBHOOK_PORT,
-        token=token,
-        ssl_cert=SSL_SERT)
     app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, ssl_context=(SSL_SERT, SSL_POM))
