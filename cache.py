841a47cdd3822b1f789a0bea3b428c93b962b8ac
Ilia Makhonin <noizemkutane77@gmail.com>
2019-01-27 21:00:03 +0300

    Update class Cache (only states). Created following functions: set_state, clear_cache, update_cache. Functions get_state, get_state_from_db and method __init__ have been updated

--- cache.py
+++ cache.py
@@ -4,15 +4,11 @@ import multiprocessing
 
 
 class Cache:
-    """
-    Cache for bot. It is need for optimization. Cache don't use now
-    I will need to add functions for work with users state
-    users: [ id: { name: <str>, state: <str>, ... }, ...] or users: { id: state }
-    Will add multiprocessing or threading for download cache data to DB (three times a day)
-    """
-    def __init__(self):
-        # Temporary storage for states of users
+    def __init__(self, lazy_loading):
         self.cache_state = dict()
+        self.lazy_loading = lazy_loading
+        if len(self.cache_state.values()) == 0:
+            self.get_state_from_db()
 
     def get_state_from_db(self):
         connection = db.get_connection()
@@ -20,9 +16,7 @@ class Cache:
             with connection.cursor() as cursor:
                 cursor.execute('SELECT user_id, state FROM users;')
                 states = cursor.fetchall()
-                cache_state = {state[0]: state[1] for state in states}
-                self.cache_state = cache_state
-                del cache_state
+                self.cache_state = {state[0]: state[1] for state in states}
         finally:
             connection.close()
 
@@ -40,19 +34,31 @@ class Cache:
 
     def get_state(self, user_id):
         try:
-            return self.cache_state.get(user_id)
-        except IndexError:
+            return self.cache_state[user_id]
+        except KeyError:
             connection = db.get_connection()
             try:
                 with connection.cursor() as cursor:
                     cursor.execute('SELECT state FROM users WHERE user_id = %s;', (user_id,))
                     new_state = cursor.fetchone()[0]
-                    self.cache_state.update([user_id, new_state])
+                    self.cache_state.update({user_id: new_state})
                     return new_state
-            except Exception as error:
-                print(error)
+            except TypeError as type_error:
+                db.logger.error(type_error)
+                return False
             finally:
                 connection.close()
 
-    def set_state(self, user_id):
-        pass
+    def set_state(self, user_id, new_state):
+        self.cache_state.update({user_id: new_state})
+        if not self.lazy_loading:
+            db.change_state(new_state, user_id)
+
+    def clear_cache(self):
+        self.cache_state.clear()
+
+    def update_cache(self):
+        if self.lazy_loading:
+            self.set_state_to_db()
+            self.clear_cache()
+            self.get_state_from_db()
