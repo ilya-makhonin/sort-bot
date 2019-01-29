72e6f9a9d6f9603b1d7d87464488eaf94cb20e04
Ilia Makhonin <noizemkutane77@gmail.com>
2019-01-29 22:49:21 +0300

    Update code, add comments and create new class instance

--- cache.py
+++ cache.py
@@ -9,6 +9,10 @@ class Cache:
             self.get_state_from_db()
 
     def get_state_from_db(self):
+        """
+        This function loads states of users from db. It"s triggered when the class is initialized.
+        :return: nothing
+        """
         connection = db.get_connection()
         try:
             with connection.cursor() as cursor:
@@ -19,6 +23,10 @@ class Cache:
             connection.close()
 
     def set_state_to_db(self):
+        """
+        This function updates the states of users in the database
+        :return: nothing
+        """
         connection = db.get_connection()
         try:
             with connection.cursor() as cursor:
@@ -31,6 +39,11 @@ class Cache:
             connection.close()
 
     def get_state(self, user_id):
+        """
+        This function gets a user state by user id. If a user state isn't in the cache that's checking in db
+        :param user_id: unique id of a users
+        :return: a state of user
+        """
         try:
             return self.cache_state[user_id]
         except KeyError:
@@ -48,15 +61,32 @@ class Cache:
                 connection.close()
 
     def set_state(self, user_id, new_state):
+        """
+        This function is setting a new user state by user id. If lazy loading is disabled it updates a user state in db
+        :param user_id: unique id of a users
+        :param new_state: a new user state
+        :return: nothing
+        """
         self.cache_state.update({user_id: new_state})
         if not self.lazy_loading:
             db.change_state(new_state, user_id)
 
     def clear_cache(self):
+        """
+        This function clears the cache
+        :return: nothing
+        """
         self.cache_state.clear()
 
     def update_cache(self):
-        if self.lazy_loading:
-            self.set_state_to_db()
-            self.clear_cache()
-            self.get_state_from_db()
+        """
+        This function updates the cache.
+        :return: nothing
+        """
+        self.set_state_to_db()
+        self.clear_cache()
+        self.get_state_from_db()
+
+
+# Initial class instance with disabling lazy loading
+cache = Cache(lazy_loading=False)
