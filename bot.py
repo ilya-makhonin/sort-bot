765c8c9a9f65c54c0e42069701c6b6fda46bcb8a
Ilia Makhonin <noizemkutane77@gmail.com>
2019-02-09 23:38:50 +0300

    Clean code and update logging

--- bot.py
+++ bot.py
@@ -6,43 +6,11 @@ import time
 import log
 import logging
 from states import get_full_state, check_section, change_state
+from utils import get_markup, pre_modified_button
 
 
 bot = telebot.TeleBot(config.token)
 logger_bot = log.logger('bot', './logs/bot.log', 'WARNING')
-
-
-def get_markup(buttons, rows=1):
-    """
-    :param buttons: buttons list type <list> like [button name, button name, ...]
-    :param rows: rows width count
-    :return: bot markup type '<telebot.types.ReplyKeyboardMarkup object at 0x000001819664CCF8>'
-    """
-    markup = telebot.types.ReplyKeyboardMarkup(True, False, row_width=rows)
-    markup.add(*buttons)
-    return markup
-
-
-def pre_modified_button(buttons,  level=False, start=0, finish=20):
-    """
-    :param buttons: buttons list type <tuple> like ((..., name)(..., name), ...)
-    :param level: to back level markup
-    :param start: start split diapason type <int>
-    :param finish: finish split diapason type <int>
-    :return: bot markup type '<telebot.types.ReplyKeyboardMarkup object at 0x000001819664CCF8>'
-    """
-    result = []
-    # ***** Create buttons list *****
-    if level is not False:
-        result.append(level)
-    for button in buttons[start:finish]:
-        result.append(button[-1])
-    if len(buttons) > finish:
-        result.append('Далее')
-    if start > 0:
-        result.append('Назад')
-    # *******************************
-    return get_markup(result)
 
 
 def next_back(message, back_to):
