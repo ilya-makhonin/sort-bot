e7726b78ce9df6562fd1c46d3ebadca4efe2aa83
Ilia Makhonin <noizemkutane77@gmail.com>
2019-03-23 23:57:44 +0300

    Full updated states module (rewrite to class) and connected to cache module, added toggle cache use

--- states.py
+++ states.py
@@ -1,47 +1,76 @@
 from sql import db
-# from cache import cache
+from cache import Cache
+from log import logger
 
 
-def check_section(user_id, state):
-    """
-    :param user_id: user id type <int>
-    :param state: checking state type <str>
-    :return: type <bull> like True or False
-    """
-    section = db.get_state(user_id)
-    # section = cache.get_state(user_id)
-    return section.split('-')[0] == state
+class States:
+    def __init__(self, use_cache):
+        self.use_cache = use_cache
+        self.logger = logger('state', './logs/state.log', 'INFO')
+        if use_cache:
+            self.cache = Cache(use_cache)
+        self.logger.info(f"Now param use_cache in Cache class instance is {self.use_cache}")
 
+    def check_section(self, user_id, state):
+        """
+        :param user_id: user id type <int>
+        :param state: checking state type <str>
+        :return: type <bull> like True or False
+        """
+        if self.use_cache:
+            section = self.cache.get_state(user_id)
+        else:
+            section = db.get_state(user_id)
+        self.logger.info(f"Used check_section: use_cache - {self.use_cache}, section var - {section}")
+        return section.split('-')[0] == state
 
-def get_full_state(user_id):
-    """
-    :param user_id: user id type <int>
-    :return: user state type <list> like [section, start page, finish page, category]
-    """
-    state = db.get_state(user_id).split('-')
-    # state = cache.get_state(user_id).split('-')
-    if len(state) == 1:
-        return state
-    if len(state) == 2:
+    def get_full_state(self, user_id):
+        """
+        :param user_id: user id type <int>
+        :return: user state type <list> like [section, start page, finish page, category]
+        """
+        if self.use_cache:
+            state = self.cache.get_state(user_id).split('-')
+        else:
+            state = db.get_state(user_id).split('-')
+
+        if len(state) == 1:
+            return state
+        if len(state) == 2:
+            pages = state[1].split(':')
+            return [state[0], int(pages[0]), int(pages[1])]
         pages = state[1].split(':')
-        return [state[0], int(pages[0]), int(pages[1])]
-    pages = state[1].split(':')
-    return [state[0], int(pages[0]), int(pages[1]), state[2]]
+        self.logger.info(f"Used get_full_state: use_cache - {self.use_cache}, "
+                         f"return data - {[state[0], int(pages[0]), int(pages[1]), state[2]]}")
+        return [state[0], int(pages[0]), int(pages[1]), state[2]]
+
+    def change_state(self, user_id, section, pages=None, category=None):
+        """
+        :param user_id: user id type <int>
+        :param section: section name type <str>
+        :param pages: pages diapason type <str> like '0:20', step - 20
+        :param category: category name type <str> like author name or theme
+        """
+        self.logger.info(f"Used change_state: use_cache - {self.use_cache}, "
+                         f"input data - {user_id, section, pages, category}")
+        if pages is None:
+            if self.use_cache:
+                self.cache.set_state(user_id, section)
+                return
+            db.change_state(section, user_id)
+        elif category is None:
+            if self.use_cache:
+                self.cache.set_state(user_id, '{}-{}'.format(section, pages))
+                return
+            db.change_state('{}-{}'.format(section, pages), user_id)
+        else:
+            if self.use_cache:
+                self.cache.set_state(user_id, '{}-{}-{}'.format(section, pages, category))
+                return
+            db.change_state('{}-{}-{}'.format(section, pages, category), user_id)
+
+    def get_cache_instance(self):
+        return self.cache
 
 
-def change_state(user_id, section, pages=None, category=None):
-    """
-    :param user_id: user id type <int>
-    :param section: section name type <str>
-    :param pages: pages diapason type <str> like '0:20', step - 20
-    :param category: category name type <str> like author name or theme
-    """
-    if pages is None:
-        db.change_state(section, user_id)
-        # cache.set_state(user_id, section)
-    elif category is None:
-        db.change_state('{}-{}'.format(section, pages), user_id)
-        # cache.set_state(user_id, '{}-{}'.format(section, pages))
-    else:
-        db.change_state('{}-{}-{}'.format(section, pages, category), user_id)
-        # cache.set_state(user_id, '{}-{}-{}'.format(section, pages, category))
+states = States(False)
